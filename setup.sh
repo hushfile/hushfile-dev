f076dc7d3658bb03ed946d8fbe6210eb78d81a6f
Ronni Elken Lindsgaard <rel@zx.dk>
2015-02-23 19:00:34 +0100

    Setup now checkouts develop branch

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 cd `dirname $0`
 
-for repo in hushfile-web hushfile-server hushfile-py-server
+for repo in hushfile-web hushfile-py-server
 do
   if [ ! -d "${repo}" ]; then
-    git clone git@github.com:hushfile/$repo.git
+    git clone git@github.com:hushfile/$repo.git -b develop
   fi
 done
